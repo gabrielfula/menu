5aabc674d245e78954b11c73e7d82add9dbb07d6
gabrielfula <gabriel.fulaneto0301@gmail.com>
2023-11-26 22:34:50 -0300

    first commit

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-full ">
+      <div className="shadow-lg p-10 bg-zinc-200 ">
+        <div className="bg-emerald-500 w-full p-10">
+          <h1>Espetos</h1>
+        </div>
+        <div className="bg-blue-500 w-full p-10">
+          <h1>Porções</h1>
+        </div>
+        <div className="bg-violet-500 w-full p-10">
+          <h1>Refrigerantes</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
