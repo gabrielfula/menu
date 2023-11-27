2249a6746a5278377660efc066ed83682d352bb3
gabrielfula <gabriel.fulaneto0301@gmail.com>
2023-11-27 07:43:44 -0300

    feat: add home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,16 +1,25 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-full ">
-      <div className="shadow-lg p-10 bg-zinc-200 ">
-        <div className="bg-emerald-500 w-full p-10">
-          <h1>Espetos</h1>
-        </div>
-        <div className="bg-blue-500 w-full p-10">
-          <h1>Porções</h1>
-        </div>
-        <div className="bg-violet-500 w-full p-10">
-          <h1>Refrigerantes</h1>
-        </div>
+    <div className="flex justify-center items-center h-full">
+      <div className="shadow-lg p-10 flex flex-col gap-3">
+        <Link href="/espetos">
+          <div className="w-full p-2 h-44 background-carne flex items-end justify-center text-zinc-50 rounded-lg">
+            <h1 className="font-bold text-2xl tracking-wide">Espetos</h1>
+          </div>
+        </Link>
+
+        <Link href="/porcoes">
+          <div className="background-porcao w-full p-2 h-44 background-porcao flex items-end justify-center rounded-lg">
+            <h1 className="font-bold text-2xl tracking-wide">Porções</h1>
+          </div>
+        </Link>
+        <Link href="/refrigerante">
+          <div className="background-porcao w-full p-2 px-32 h-44 background-refrigerante flex items-end justify-center rounded-lg">
+            <h1 className="font-bold text-2xl tracking-wide">Refrigerantes</h1>
+          </div>
+        </Link>
       </div>
     </div>
   );
