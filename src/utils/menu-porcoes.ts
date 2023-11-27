1c215fd5b05285345a5ca678a7c1d5c76dcfaab8
gabrielfula <gabriel.fulaneto0301@gmail.com>
2023-11-27 09:19:39 -0300

    fix: path imagens porcoes

--- src/utils/menu-porcoes.ts
+++ src/utils/menu-porcoes.ts
@@ -4,16 +4,22 @@ export const porcoes: MenuType[] = [
   {
     id: 1,
     title: "Batata Frita",
+    img: "imagens/batata.png",
+    price: 9,
     desc: "Saboreie a perfeição crocante em cada mordida! Nossas batatas fritas são a essência do delicioso - crocantes por fora, macias por dentro, e preparadas com um toque especial que as torna simplesmente irresistíveis.",
   },
   {
     id: 2,
     title: "Mandioca Frita",
+    img: "imagens/batata.png",
+    price: 9,
     desc: "Descubra a verdadeira essência do sabor com nossas mandiocas fritas excepcionais! Cada porção é cuidadosamente preparada para oferecer a combinação perfeita de crocância e maciez, envolvendo seu paladar em uma experiência única.",
   },
   {
     id: 3,
     title: "Polenta Frita",
+    img: "imagens/batata.png",
+    price: 9,
     desc: "Renda-se ao sabor autêntico da nossa polenta frita, Descubra a polenta frita que vai além das expectativas e mergulhe numa viagem de sabores inesquecíveis",
   },
 ];
