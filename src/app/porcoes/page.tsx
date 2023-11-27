258d8ebdc96de74f84a757e779cb139855c437eb
gabrielfula <gabriel.fulaneto0301@gmail.com>
2023-11-27 09:20:13 -0300

    test: test porcoes page

--- src/app/porcoes/page.tsx
+++ src/app/porcoes/page.tsx
@@ -1,7 +1,21 @@
+import MenuIndividual from "@/components/MenuIndividual";
+import { porcoes } from "@/utils/menu-porcoes";
+
 export default function Porções() {
   return (
-    <div>
-      <p>Porções</p>
-    </div>
+    <>
+      <div className="flex justify-center flex-col items-center h-full gap-5">
+        {porcoes.map((item) => {
+          return (
+            <MenuIndividual
+              key={item.id}
+              img={item.img}
+              title={item.title}
+              price={item.price}
+            />
+          );
+        })}
+      </div>
+    </>
   );
 }
