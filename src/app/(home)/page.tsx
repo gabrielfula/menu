5427443a7f1309d0399b67dc089b5524a31da29d
gabrielfula <gabriel.fulaneto0301@gmail.com>
2023-11-27 07:55:14 -0300

    style: responsive mobile

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,25 +2,24 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-full">
-      <div className="shadow-lg p-10 flex flex-col gap-3">
-        <Link href="/espetos">
-          <div className="w-full p-2 h-44 background-carne flex items-end justify-center text-zinc-50 rounded-lg">
+    <>
+      <div className="flex flex-col gap-2 justify-center h-full items-center overflow-hidden">
+        <Link href="/espetos" className="w-full flex justify-center">
+          <div className="w-4/6 p-2 h-44 background-carne flex items-end justify-center text-zinc-50 rounded-lg ">
             <h1 className="font-bold text-2xl tracking-wide">Espetos</h1>
           </div>
         </Link>
-
-        <Link href="/porcoes">
-          <div className="background-porcao w-full p-2 h-44 background-porcao flex items-end justify-center rounded-lg">
+        <Link href="/porcoes" className="w-full flex justify-center">
+          <div className="background-porcao w-4/6 p-2 h-44 background-porcao flex items-end justify-center rounded-lg">
             <h1 className="font-bold text-2xl tracking-wide">Porções</h1>
           </div>
         </Link>
-        <Link href="/refrigerante">
-          <div className="background-porcao w-full p-2 px-32 h-44 background-refrigerante flex items-end justify-center rounded-lg">
+        <Link href="/refrigerante" className="w-full flex justify-center">
+          <div className="background-porcao w-4/6 p-2 px-32 h-44 background-refrigerante flex items-end justify-center rounded-lg">
             <h1 className="font-bold text-2xl tracking-wide">Refrigerantes</h1>
           </div>
         </Link>
       </div>
-    </div>
+    </>
   );
 }
